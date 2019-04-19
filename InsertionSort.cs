eb82ca2decc80b8048856b3d39ca48e0d539962d
Unknown <codyeboucher@gmail.com>
2019-04-18 22:29:10 -0500

    InsertionSort
    Insertion sort implementation

---
+++ InsertionSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorts
+{
+    class InsertionSort
+    {
+        public static void Sort(int[] nums)
+        {
+            for(int i = 1; i < nums.Length; i++)
+            {
+                var key = nums[i];
+                var k = i;
+                    while (k > 0 && key < nums[k - 1])
+                    {
+                        nums[k] = nums[k - 1];
+                        k--;
+                    }
+                nums[k] = key;
+            }
+        }
+    }
+}
