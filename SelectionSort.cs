8fe70cc9a8fca09c920d24197d3f07108a81b969
Unknown <codyeboucher@gmail.com>
2019-04-18 21:02:01 -0500

    SelectionSort
    Selection sort implementation

---
+++ SelectionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorts
+{
+    class SelectionSort
+    {
+        public static void Sort(int[] nums)
+        {
+            for(int i = 0; i < nums.Length; i++)
+            {
+                var min = i;
+                for(int k = i+1; k < nums.Length; k++)
+                {
+                    if(nums[k] < nums[min])
+                    {
+                        min = k;
+                    }
+                }
+                if(min != i)
+                {
+                    var temp = nums[i];
+                    nums[i] = nums[min];
+                    nums[min] = temp;
+                }
+            }
+        }
+    }
+}
