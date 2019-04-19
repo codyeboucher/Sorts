45b99ea405284b4f65aba4240422b7e6f043fbd4
Unknown <codyeboucher@gmail.com>
2019-04-18 19:38:52 -0500

    BubbleSort
    Bubble sort implementation

---
+++ BubbleSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorts
+{
+    class BubbleSort
+    {
+        public static void Sort(int[] nums)
+        {
+            for(int i = nums.Length-1; i > 0; i--)
+            {
+                for(int k = 0; k < i; k++)
+                {
+                    if(nums[k] > nums[k + 1])
+                    {
+                        var temp = nums[k];
+                        nums[k] = nums[k + 1];
+                        nums[k + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
