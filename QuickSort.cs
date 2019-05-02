567193ad75da4a5a4fc682c995f268513fb51838
Cody <codyeboucher@gmail.com>
2019-05-01 20:11:06 -0500

    QuickSort
    Quick sort implementation

---
+++ QuickSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorts
+{
+    class QuickSort
+    {
+        public static void Sort(int[] nums, int start, int end)
+        {
+            if(start < end)
+            {
+                var pivot = Partition(nums, start, end);
+                Sort(nums, start, pivot - 1);
+                Sort(nums, pivot + 1, end);
+            }
+        }
+
+        private static int Partition(int[] nums, int start, int end)
+        {
+            int index = start;
+            int pivot = nums[end];
+
+            for(int i = start; i < end; i++)
+            {
+                if(nums[i] < pivot)
+                {
+                    Swap(nums, nums[i], nums[index]);
+                    index++;
+                }
+            }
+
+            Swap(nums, index, end);
+            return index;
+        }
+
+        private static void Swap(int[] nums, int first, int second)
+        {
+            var temp = nums[first];
+            nums[first] = nums[second];
+            nums[second] = temp;
+        }
+    }
+}
